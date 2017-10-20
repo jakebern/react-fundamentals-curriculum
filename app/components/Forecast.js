5ebb70fee9ad7cab25064424d9ff2c94101f22d5
Jacob Bernstein <jacobbernstein@Jacobs-MacBook-Pro.local>
2017-10-20 22:00:57 +0900

    calling forecast API

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,7 +1,16 @@
 var React = require('react');
+var queryString = require('query-string');
+var api = require('../utils/api');
 
 class Forecast extends React.Component {
+  componentDidMount(){
+  	var queryparam = queryString.parse(this.props.location.search);
+  	api.getMyWeather(queryparam.city).then(function(results){
+  		console.log(results);
+  	})
+  }
   render() {
+
     return (
       <div className = "header">
         This is the forecast
