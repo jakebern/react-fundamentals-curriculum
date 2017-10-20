3254299c908a1070fd4804d707222932da614c66
Jacob Bernstein <jacobbernstein@Jacobs-MacBook-Pro.local>
2017-10-20 21:49:18 +0900

    New Fancy Bind function

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,34 +1,42 @@
 var React = require('react');
 var Input = require('./Input');
 
-class Home extends React.Component {
-  handleSubmit(city){
-    console.log("blah");
-  };
+function onSubmit(props, city) {
+    props.history.push({
+      pathname: 'forecast',
+      search: '?city=' + city
+    });
+  }
 
-  render() {
-    return (
-      <div>
-         <div className = "header">
-            <div>
-              <h2>Clever Title</h2>
-            </div>
-            <div>
-              <Input handleSubmit = {this.handleSubmit.bind(this)}/>
-            </div>
-          </div>
-
+function Home(props) {
+  return (
+    <div>
+       <div className = "header">
           <div>
-            <div className = "entry">
-              <h3>Enter a City and State</h3>
-              <Input handleSubmit = {this.handleSubmit.bind(this)}/>
-            </div>
+            <h2>Clever Title</h2>
           </div>
+          <div>
+            <Input handleSubmit = {
+              //passes in current props to function to call when needed
+              onSubmit.bind(null, props)
+              }
+            />
+          </div>
+        </div>
 
+        <div>
+          <div className = "entry">
+            <h3>Enter a City and State</h3>
+
+            <Input handleSubmit = {
+              //passes in current props to function to call when needed
+              onSubmit.bind(null, props)
+            }/>
+
+          </div>
+        </div>
       </div>
-
     )
   }
-}
 
 module.exports = Home;
