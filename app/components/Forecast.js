9c8203bfc2406942c68cc9492f534231f97562b6
Jacob Bernstein <jacobbernstein@Jacobs-MacBook-Pro.local>
2017-10-21 09:50:53 +0900

    Fix API route:

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -3,23 +3,35 @@ var queryString = require('query-string');
 var api = require('../utils/api');
 
 class Forecast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true,
+      data: null
+    }
+  }
   componentDidMount(){
-  	var queryparam = queryString.parse(this.props.location.search);
-  	api.getMyWeather(queryparam.city).then(function(results){
-			console.log(results)
-  	})
+  	var city = queryString.parse(this.props.location.search).city;
+  	api.getMyWeather(city).then(function(results){
+			console.log(results);
+      this.setState(function(){
+        return {
+          loading: false,
+          data: results
+        }
+      })
+    }.bind(this))
   	.catch(function(error){
   		console.log("This is the error: " + error)
   	})
   }
 
   render() {
-    return (
-      <div className = "header">
-        This is the forecast
-      </div>
-
-    )
+    return this.state.loading === true 
+    ? <div> Loading </div>
+    : <div> 
+      My daily forecast goes here.
+    </div>
   }
 }
 
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,10 +1,11 @@
 var axios = require('axios');
 
 var id = "1806ff6ef1ea14fd9714655df80b3ecd";
+var base_url = "http://api.openweathermap.org/data/2.5/";
 
 module.exports = {
 	getMyWeather: function(city) {
-		return axios.get("http://api.openweathermap.org/data/2.5/weather?q="+city+"&type=accurate&APPID=" + id)
+		return axios.get(base_url + "forecast?q="+city+"&type=accurate&APPID=" + id + "&cnt=5")
     .then(function(response){
       return response.data;
     });
